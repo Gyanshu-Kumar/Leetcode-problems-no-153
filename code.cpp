243a6d513b527b7639b9d0684aa0562e44e8e7cb
Gyanshu Kumar <119155250+Gyanshu-Kumar@users.noreply.github.com>
2023-08-12 21:19:51 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,10 +1,30 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello World";
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+        int start = 0;
+        int end = nums.size() - 1;
+            while(start < end){
+                int mid = (start+end)/2;
 
-    return 0;
+                if (nums[mid] < nums[end]){
+                    end = mid;
+
+                }
+                else{
+                    start = mid + 1;
+                }
+                
+
+                
+
+            }
+            return nums[start];
+        
+        }
+
+
+        
     
-} // namespace std;
+};
 
